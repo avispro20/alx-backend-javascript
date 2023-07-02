c35b03862b2761c4ce5d331016b02448d128a4fb
avispro20 <solomonok07@gmail.com>
2023-07-02 15:24:45 -0700

    ES6 Classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+//Implement a class,  ClassRoom
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+//Import the ClassRoom class
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [19, 20, 34].map((room) => new ClassRoom(room));
+}
