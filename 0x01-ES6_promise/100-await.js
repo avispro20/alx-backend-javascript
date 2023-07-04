ad6ea9f5d565a81a746483d2da69538207f0f580
avispro20 <solomonok07@gmail.com>
2023-07-04 16:03:05 -0700

    ES6 promises

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+// Import uploadPhoto and createUs from utils
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    return {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (_) {
+    return { photo: null, user: null };
+  }
+}
